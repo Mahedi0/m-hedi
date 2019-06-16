d2f4f2f49108dd6ede6a34bc209bda0df8caaa17
Mahedi0 <50787639+Mahedi0@users.noreply.github.com>
2019-06-16 11:35:21 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DATABASE_RELATION_APP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Savebutton1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Savebutton1
+            // 
+            this.Savebutton1.Location = new System.Drawing.Point(615, 121);
+            this.Savebutton1.Name = "Savebutton1";
+            this.Savebutton1.Size = new System.Drawing.Size(98, 33);
+            this.Savebutton1.TabIndex = 0;
+            this.Savebutton1.Text = "Save";
+            this.Savebutton1.UseVisualStyleBackColor = true;
+            this.Savebutton1.Click += new System.EventHandler(this.Savebutton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Savebutton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Savebutton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DATABASE_RELATION_APP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        private void Savebutton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //1
+                SqlConnection sqlConnection = new SqlConnection();
+                string connectionstring = @"Server=PC-301-14\SQLEXPRESS; Database=STUDENTBD; Integrated Security)";
+                sqlConnection.ConnectionString=connectionstring;
+
+                //2
+                SqlCommand sqlCommand=new SqlCommand();
+                string commandstring=@"INSERT INTO DEPARTMENT(Name,Code)Values('Computer Science & Engineering)";
+                sqlCommand.CommandText =commandstring;
+                sqlCommand.Connection = sqlConnection;
+
+                //3
+                sqlConnection.Open();
+
+                //4
+                int isExecuted = 0;
+                isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted>0)
+                {
+                    MessageBox.Show("Saved Successfully");
+                
+
+                }
+                else
+                {
+                    MessageBox.Show("Save FAiled");
+                }
+
+
+            }   
+
+
+
+                 
+            
+        }
+    }
+}
